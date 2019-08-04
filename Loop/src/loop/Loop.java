2ad000664384de06eb1e01768ad859435855b0b8
ferna <ferna@192.168.56.1>
2019-08-04 20:21:08 -0300

    Modelo 1

---
+++ Loop/src/loop/Loop.java
@@ -0,0 +1,18 @@
+
+package loop;
+
+
+public class Loop {
+
+    
+    public static void main(String[] args) {
+        
+          int contador = 0;
+        
+        while(contador < 500) {
+    contador++;
+    System.out.println (contador);
+        }
+    }
+    
+}
